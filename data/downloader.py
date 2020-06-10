cc48e84113390d1d03b21b8df8080fdd41b8e50a
Peter Devine <pdev438@UoA.auckland.ac.nz>
2020-06-10 16:59:23 +1200

    Make data downloader syntax neat with other modules

--- data/downloader.py
+++ data/downloader.py
@@ -3,7 +3,7 @@ from data.guzman_2015 import Guzman2015
 
 def get_data(list_of_data, random_state):
 
-    downloaders = {
+    DOWNLOADER_DICT = {
         "guzman_2015": Guzman2015
     }
 
@@ -12,5 +12,5 @@ def get_data(list_of_data, random_state):
     for dataset in list_of_data:
         dataset_download_dir = os.path.join(DOWNLOAD_DIR, dataset)
         if not os.path.exists(dataset_download_dir):
-            downloader = downloaders[dataset](random_state = random_state, download_dir = dataset_download_dir)
+            downloader = DOWNLOADER_DICT[dataset](random_state = random_state, download_dir = dataset_download_dir)
             downloader.download()
