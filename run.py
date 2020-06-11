2432fdf271876bb68cfc30dced4f7e89d2a4a8ad
Peter Devine <pdev438@UoA.auckland.ac.nz>
2020-06-11 17:01:39 +1200

    Run visualisation and clustering in run

--- run.py
+++ run.py
@@ -2,6 +2,14 @@ from data.downloader import get_data
 
 from mapping.mapper import map_data
 
+from visualisation.visualise import visualise_data
+
+from clustering.clusterer import cluster_data
+
 get_data(["guzman_2015"], random_state=111)
 
 map_data(["guzman_2015"], ["USE"])
+
+visualise_data(["guzman_2015"])
+
+cluster_data(["guzman_2015"])
