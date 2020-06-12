91cffa5e3cd32d1cc19a28471fcc7c79ca74e264
Peter Devine <pdev438@UoA.auckland.ac.nz>
2020-06-12 15:53:14 +1200

    Add bert vanilla and tfidf pca embeddings to projcet

---
+++ mapping/mapping_models/bert_vanilla.py
@@ -0,0 +1,58 @@
+import math
+
+import pandas as pd
+
+import numpy as np
+from tqdm import tqdm
+
+import torch
+from transformers import AutoTokenizer, AutoModel
+from mapping.mapping_models.mapping_models_base import BaseMapper
+
+class BertVanillaMapper(BaseMapper):
+
+    def get_embeds(self):
+        df = self.get_dataset(self.test_dataset, split="test")
+
+        MODEL_NAME = 'roberta-large'
+
+        # Load pre-trained model tokenizer (vocabulary)
+        tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME, use_fast=True)
+
+        # Load the BERT model
+        model = AutoModel.from_pretrained(MODEL_NAME)
+
+        MAX_LENGTH = 128
+        BATCH_SIZE = 64
+
+        # Tokenize and convert to input IDs
+        tokens_tensor = tokenizer.batch_encode_plus(list(df.text.values), max_length = MAX_LENGTH, pad_to_max_length=True, return_tensors="pt")
+        tokens_tensor = tokens_tensor["input_ids"]
+
+        # Create list for all embeddings to be saved to
+        embeddings = []
+
+        # Get the number of observations to embed
+        num_obs = tokens_tensor.shape[0]
+
+        # Batch tensor so we can iterate over inputs
+        test_loader = torch.utils.data.DataLoader(tokens_tensor, batch_size=BATCH_SIZE, shuffle=False)
+
+        # Make sure the torch algorithm runs without gradients (as we aren't training)
+        with torch.no_grad():
+            print(f"Iterating over inputs {MODEL_NAME} vanilla")
+            # Iterate over all batches, passing the batches through the
+            for test_batch in tqdm(test_loader):
+                # See the models docstrings for the detail of the inputs
+                outputs = model(test_batch)
+                # Output the final average encoding across all characters as a numpy array
+                np_array = outputs[0].mean(dim=1).numpy()
+                # Append this encoding to a list
+                embeddings.append(np_array)
+
+        all_embeddings = np.concatenate(embeddings, axis=0)
+
+        return all_embeddings, df.label
+
+    def get_mapping_name(self, test_dataset):
+        return "bert_vanilla"
