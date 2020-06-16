145a2a3be8c2b5f4826be44e1c7a4835d358f7ee
Peter Devine <pdev438@UoA.auckland.ac.nz>
2020-06-16 17:11:17 +1200

    Add LDA and Glove to mapping.py

--- mapping/mapper.py
+++ mapping/mapper.py
@@ -1,17 +1,21 @@
 from mapping.mapping_models.use import UseMapper
 from mapping.mapping_models.bert_vanilla import BertVanillaMapper
 from mapping.mapping_models.tfidf_pca import TfidfPcaMapper
+from mapping.mapping_models.lda import LdaMapper
 from mapping.mapping_models.glove import GloveMapper
 from mapping.mapping_models.sbert import SBertMapper
 from mapping.mapping_models.sbert_wk import SBertWKMapper
+from mapping.mapping_models.gem import GemMapper
 
 MAPPER_DICT = {
     "use": UseMapper,
     "bert_vanilla": BertVanillaMapper,
     "tfidf_pca": TfidfPcaMapper,
+    "lda": LdaMapper,
     "glove": GloveMapper,
     "sbert": SBertMapper,
     "sbert_wk": SBertWKMapper,
+    "gem": GemMapper,
 }
 
 def map_data(list_of_datasets, list_of_mappings):
