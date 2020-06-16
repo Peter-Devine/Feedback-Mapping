db71c2260a77f8e804f81f6873dd677085f8fb56
Peter Devine <pdev438@UoA.auckland.ac.nz>
2020-06-16 14:15:23 +1200

    Add extra colours to viz

--- visualisation/visualise.py
+++ visualisation/visualise.py
@@ -11,20 +11,24 @@ from sklearn.decomposition import PCA
 from clustering.clusterer import get_embeddings_paths, get_embedding_data
 from utils.utils import create_dir
 
-def visualise_data(dataset_names):
+def visualise_data(dataset_names, list_of_embeddings):
 
     for dataset_name in dataset_names:
         # Get all embedding files for given dataset
         embedding_files_data = get_embeddings_paths(dataset_name)
 
         for embedding_file, embedding_name in embedding_files_data:
-            # From each embedding file, takeout the numerical embeddings and labels
-            embeddings, labels = get_embedding_data(embedding_file)
-            # Map the high dim embeddings to a 2D space
-            twoD_mapping_df = get_PCA_two_D_df(embeddings, labels)
-            # Output the resulting 2D space to a graph
-            graph_PCA_data(twoD_mapping_df, dataset_name, embedding_name)
+            if embedding_name in list_of_embeddings:
+                # Create the visulisation for each embedding and dataset
+                visualise_single_dataset(dataset_name, embedding_file, embedding_name)
 
+def visualise_single_dataset(dataset_name, embedding_file, embedding_name):
+    # From each embedding file, takeout the numerical embeddings and labels
+    embeddings, labels = get_embedding_data(embedding_file)
+    # Map the high dim embeddings to a 2D space
+    twoD_mapping_df = get_PCA_two_D_df(embeddings, labels)
+    # Output the resulting 2D space to a graph
+    graph_PCA_data(twoD_mapping_df, dataset_name, embedding_name)
 
 def get_PCA_two_D_df(embeddings, labels):
     # Initialize the PCA model to map multi dimensional embeddings to simple 2D embedding
@@ -42,12 +46,20 @@ def get_PCA_two_D_df(embeddings, labels):
     return twoD_mapping_df
 
 def get_colour_map(unique_labels):
+    # Set some colours that contrast nicely with eachother
+    set_colours = ["k", "r", "y", "b", "m", "g", "c", "orange", "silver", "purple", "greenyellow", "cadetblue", "sienna"]
     # Get the number of unique labels in the dataset
     num_labels = len(unique_labels)
     # Get the RGB values of colours moving successively along the HSV spectrum
     colours = cm.hsv(np.linspace(0, 1, num_labels))
     # Convert RGB values to hex strings
     colours = ["#%02x%02x%02x" % (int(r*255), int(g*255), int(b*255)) for r,g,b,a in colours]
+    # add the set colours to the hex colours
+    if num_labels <= len(set_colours):
+        colours = set_colours[:num_labels]
+    else:
+        extra_colours = num_labels - len(set_colours)
+        colours = set_colours + colours[:extra_colours]
     # Map each hex string to a certain unique label
     colour_map = {label:colour for label, colour in zip(unique_labels, colours)}
     return colour_map
