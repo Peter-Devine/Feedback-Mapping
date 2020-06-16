ef407854426412f7cd310b0f76175101e5dc399d
Peter Devine <pdev438@UoA.auckland.ac.nz>
2020-06-16 14:17:20 +1200

    Add embeddings to mapper and refactor to do one embedding at a time

--- mapping/mapper.py
+++ mapping/mapper.py
@@ -1,18 +1,27 @@
 from mapping.mapping_models.use import UseMapper
 from mapping.mapping_models.bert_vanilla import BertVanillaMapper
 from mapping.mapping_models.tfidf_pca import TfidfPcaMapper
+from mapping.mapping_models.glove import GloveMapper
+from mapping.mapping_models.sbert import SBertMapper
+from mapping.mapping_models.sbert_wk import SBertWKMapper
 
 MAPPER_DICT = {
     "use": UseMapper,
     "bert_vanilla": BertVanillaMapper,
-    "tfidf_pca": TfidfPcaMapper
+    "tfidf_pca": TfidfPcaMapper,
+    "glove": GloveMapper,
+    "sbert": SBertMapper,
+    "sbert_wk": SBertWKMapper,
 }
 
 def map_data(list_of_datasets, list_of_mappings):
     for dataset in list_of_datasets:
         for mapping in list_of_mappings:
-            mapper = get_mapper(mapping, dataset)
-            mapper.embed()
+            embed_single_dataset(mapping, dataset)
+
+def embed_single_dataset(mapping, dataset):
+    mapper = get_mapper(mapping, dataset)
+    mapper.embed()
 
 def get_mapper(mapping_type, dataset):
     mapping_type = mapping_type.lower()
