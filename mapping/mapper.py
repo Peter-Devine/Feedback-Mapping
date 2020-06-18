c9e043d454b81471615b319c292f3ff088e89948
Peter Devine <pdev438@UoA.auckland.ac.nz>
2020-06-18 14:46:10 +1200

    Add ensemble to the project

--- mapping/mapper.py
+++ mapping/mapper.py
@@ -7,6 +7,8 @@ from mapping.mapping_models.sbert import SBertMapper
 from mapping.mapping_models.sbert_wk import SBertWKMapper
 from mapping.mapping_models.gem import GemMapper
 from mapping.mapping_models.t5_vanilla import T5VanillaMapper
+from mapping.mapping_models.ensemble import EnsembleMapper
+from mapping.mapping_models.bert_cls_trained import BertClsTrainedMapper
 
 MAPPER_DICT = {
     "use": UseMapper,
@@ -18,6 +20,8 @@ MAPPER_DICT = {
     "sbert_wk": SBertWKMapper,
     "gem": GemMapper,
     "t5_vanilla": T5VanillaMapper,
+    "ensemble": EnsembleMapper,
+    "bert_cls_trained": BertClsTrainedMapper,
 }
 
 def map_data(list_of_datasets, list_of_mappings):
---
+++ mapping/mapping_models/ensemble.py
@@ -0,0 +1,25 @@
+from clustering.clusterer import get_embeddings_paths, get_embedding_data
+
+import numpy as np
+
+from mapping.mapping_models.mapping_models_base import BaseMapper
+
+class EnsembleMapper(BaseMapper):
+
+    def get_embeds(self):
+        embedding_files_data = get_embeddings_paths(self.test_dataset)
+
+        concatenated_embedding = None
+        for embedding_file, embedding_name in embedding_files_data:
+
+            embeddings, labels = get_embedding_data(embedding_file)
+
+            if concatenated_embedding is None:
+                concatenated_embedding = embeddings
+            else:
+                concatenated_embedding = np.concatenate([concatenated_embedding,embeddings], axis=1)
+
+        return concatenated_embedding, labels
+
+    def get_mapping_name(self, test_dataset):
+        return f"ensemble"
