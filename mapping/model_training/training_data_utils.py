2b073ded043a5303f2fb349070038009232ccf35
unknown <pdev438@aucklanduni.ac.nz>
2020-06-19 21:20:48 +1200

    Fix nsp dataset column name

--- mapping/model_training/training_data_utils.py
+++ mapping/model_training/training_data_utils.py
@@ -35,7 +35,7 @@ def get_next_sentence_df(df):
     unmatched_df = matched_df.sample(frac=0.5, random_state = get_random_seed())
     matched_df = matched_df.drop(unmatched_df.index)
 
-    unmatched_second_text_df = unmatched_df.apply(lambda x: unmatched_df[unmatched_df.label != x["id"]].sample(1, random_state = get_random_seed()).iloc[0], axis=1)
+    unmatched_second_text_df = unmatched_df.apply(lambda x: unmatched_df[unmatched_df.id != x["id"]].sample(1, random_state = get_random_seed()).iloc[0], axis=1)
 
     unmatched_df["second_text"] = unmatched_second_text_df["second_text"]
 
