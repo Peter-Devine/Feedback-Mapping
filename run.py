79adacd42434c74b40799dcf07042c35e61c9828
Peter-Devine <49399312+Peter-Devine@users.noreply.github.com>
2020-06-25 18:13:37 +1200

    Add all datasets to run code

--- run.py
+++ run.py
@@ -8,11 +8,10 @@ from clustering.clusterer import cluster_data
 
 from utils.utils import get_random_seed, set_random_seed
 
-datasets = ["guzman_2015", "williams_2017"]
-embeddings = ["bert_cls_trained"]
+datasets = ["guzman_2015", "maalej_2016", "williams_2017", "chen_2014", "di_sorbo_2016", "scalabrino_2017", "jha_2017", "tizard_2019"]
+embeddings = ["sbert"]
 
 set_random_seed(111)
-print("!!!!!!!!!!!")
 get_data(datasets)
 
 map_data(datasets, embeddings)
