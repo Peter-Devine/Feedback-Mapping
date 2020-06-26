db553ef7e9260969989eb495fba8881f843bc54f
Peter-Devine <49399312+Peter-Devine@users.noreply.github.com>
2020-06-26 15:53:59 +1200

    Add score collator to run.py

--- run.py
+++ run.py
@@ -6,12 +6,15 @@ from visualisation.visualise import visualise_data
 
 from clustering.clusterer import cluster_data
 
+from results.score_collator import collate_scores
+
 from utils.utils import get_random_seed, set_random_seed
 
 datasets = ["guzman_2015", "maalej_2016", "williams_2017", "chen_2014", "di_sorbo_2016", "scalabrino_2017", "jha_2017", "tizard_2019"]
-embeddings = ["sbert"]
+embeddings = ["use",  "tfidf_pca", "glove", "bert_vanilla", "lda", "gem"]#"tfidf_pca", "glove", "sbert_wk", "ensemble"]
 
 set_random_seed(111)
+
 get_data(datasets)
 
 map_data(datasets, embeddings)
@@ -19,3 +22,5 @@ map_data(datasets, embeddings)
 visualise_data(datasets, embeddings)
 
 cluster_data(datasets, embeddings)
+
+collate_scores()
