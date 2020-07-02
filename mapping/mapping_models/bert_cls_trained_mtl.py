692cffca48fa2f23aa7707cefcebcbfd36541499
Peter-Devine <49399312+Peter-Devine@users.noreply.github.com>
2020-07-02 14:47:40 +1200

    Add MTL CLS bert and bert util inference code

---
+++ mapping/mapping_models/bert_cls_trained_mtl.py
@@ -0,0 +1,63 @@
+import pandas as pd
+import os
+
+from tqdm import tqdm
+
+import torch
+from transformers import AutoModel
+from mapping.mapping_models.mapping_models_base import BaseMapper
+from mapping.model_training.transformer_training import train_cls
+from utils.bert_utils import get_lm_embeddings
+
+class BertClsTrainedMtlMapper(BaseMapper):
+
+    def get_embeds(self):
+        test_df = self.get_dataset(self.test_dataset, split="test")
+
+        all_embeddings = get_lm_embeddings(self, test_df, f"{self.test_dataset} cls trained")
+
+        return all_embeddings, test_df.label
+
+    def set_parameters(self):
+        self.model_name = 'binwang/bert-base-nli'
+        self.max_length = 128
+        self.batch_size = 64
+
+    def get_model(self):
+        model_path = os.path.join(self.model_dir, f"{self.test_dataset}.pt")
+
+        model = self.read_or_create_model(model_path)
+
+        return model
+
+    def train_model(self, model_path):
+        train_dfs = self.get_all_datasets(split="train")
+        valid_dfs = self.get_all_datasets(split="val")
+
+        def get_concat_next_sentence_df(dfs):
+            concat_df = None
+
+            for dataset_name, dataset_df in dfs.items():
+                if concat_df is None:
+                    concat_df = dataset_df
+                else:
+                    concat_df = concat_df.append(dataset_df)
+            return concat_df
+
+        train_df = get_concat_next_sentence_df(train_dfs)
+        valid_df = get_concat_next_sentence_df(valid_dfs)
+
+        params = {
+            "lr": 5e-5,
+            "eps": 1e-6,
+            "wd": 0.01,
+            "epochs": 5,
+            "patience": 2
+        }
+
+        model = train_cls(train_df, valid_df, self.model_name, self.batch_size, self.max_length, self.device, params)
+
+        torch.save(model.state_dict(), model_path)
+
+    def get_mapping_name(self, test_dataset):
+        return f"bert_cls_trained"
---
+++ utils/bert_utils.py
@@ -0,0 +1,46 @@
+import torch
+import numpy as np
+from transformers import AutoTokenizer
+from tqdm import tqdm
+
+def get_lm_embeddings(mapper_model, test_df, trained_model_name):
+    mapper_model.set_parameters()
+
+    # Load pre-trained model tokenizer (vocabulary)
+    tokenizer = AutoTokenizer.from_pretrained(self.model_name, use_fast=True)
+
+    # Load the BERT model
+    model = mapper_model.get_model()
+    model = model.to(mapper_model.device)
+    model.eval()
+    model.zero_grad()
+
+    # Tokenize and convert to input IDs
+    tokens_tensor = tokenizer.batch_encode_plus(list(test_df.text.values),
+                                                max_length = mapper_model.max_length,
+                                                pad_to_max_length=True,
+                                                truncation=True,
+                                                return_tensors="pt")
+    tokens_tensor = tokens_tensor["input_ids"]
+
+    # Create list for all embeddings to be saved to
+    embeddings = []
+
+    # Batch tensor so we can iterate over inputs
+    test_loader = torch.utils.data.DataLoader(tokens_tensor, batch_size=mapper_model.batch_size, shuffle=False)
+
+    # Make sure the torch algorithm runs without gradients (as we aren't training)
+    with torch.no_grad():
+        print(f"Iterating over inputs {trained_model_name}")
+        # Iterate over all batches, passing the batches through the
+        for test_batch in tqdm(test_loader):
+            # See the models docstrings for the detail of the inputs
+            outputs = model(test_batch.to(mapper_model.device))
+            # Output the final average encoding across all characters as a numpy array
+            np_array = outputs[0].mean(dim=1).cpu().numpy()
+            # Append this encoding to a list
+            embeddings.append(np_array)
+
+    all_embeddings = np.concatenate(embeddings, axis=0)
+
+    return all_embeddings
