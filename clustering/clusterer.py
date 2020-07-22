77db961d82580ea1fee517101ccb5ba773e566a8
Peter-Devine <49399312+Peter-Devine@users.noreply.github.com>
2020-07-22 13:52:49 +1200

    Add knn similarity score

--- clustering/clusterer.py
+++ clustering/clusterer.py
@@ -1,6 +1,7 @@
 import os
 import pandas as pd
 import numpy as np
+import scipy
 
 from sklearn.cluster import KMeans
 from sklearn.metrics import homogeneity_score
@@ -30,7 +31,7 @@ def cluster_single_dataset(dataset_name, embedding_file, embedding_name):
     preds, kmeans = cluster_kmeans(embeddings, labels)
 
     # Score predictions compared to golds
-    scores = score_clustering(labels, preds)
+    scores = score_clustering(labels, preds, embeddings)
 
     # Get the compositions of each cluster
     compositions = get_cluster_compositions(labels, preds)
@@ -128,12 +129,34 @@ def get_avg_max_fraction(labels, preds):
 
     return sum(max_fractions) / len(max_fractions)
 
-def score_clustering(labels, preds):
+def get_knn_similarity_score(labels, embeddings, k=5):
+    # Get the distances between each embedding
+    mutual_distances = scipy.spatial.distance.squareform(scipy.spatial.distance.pdist(embeddings))
+
+    # Find the arguments of the k closest points
+    closest_args = [dist.argsort()[:k] for dist in mutual_distances]
+
+    # Get a dict, mapping arguments to labels
+    label_dict = {i: label for i, label in enumerate(labels)}
+
+    # For each kth closest value, find the number of points which have the same label
+    total_number_similar_neighbours = 0
+    for kth_closest in range(k):
+        closest_labels = np.vectorize(label_dict.get)(closest_args)[:,kth_closest]
+        total_number_similar_neighbours += (closest_labels == labels).sum()
+
+    # Get the average number of neighbours that have identical labels to a point
+    avg_similar_neighbours = total_number_similar_neighbours / len(labels)
+
+    return avg_similar_neighbours
+
+def score_clustering(labels, preds, embeddings):
     # Score clustering predictions compared to real labels
     scorings = {}
     scorings["homogeneity"] = homogeneity_score(labels, preds)
     scorings["avg_max_frac"] = get_avg_max_fraction(labels, preds)
     scorings["num_clusters"] = len(np.unique(preds))
+    scorings["knn_sim"] = get_knn_similarity_score(labels, embeddings, k=5)
     return pd.DataFrame(scorings, index=["scorings"])
 
 def get_cluster_compositions(labels, preds):
