ca75f7195c65dfdcd842f63c39d54fbe89b5aa14
Peter-Devine <49399312+Peter-Devine@users.noreply.github.com>
2020-08-06 17:30:22 +1200

    Add eval batch size to all vanilla mappers

--- mapping/mapping_models/data_blind_models/bert_likes/bert_vanilla_large.py
+++ mapping/mapping_models/data_blind_models/bert_likes/bert_vanilla_large.py
@@ -5,7 +5,7 @@ class BertVanillaLargeMapper(BertVanillaMapper):
     def set_parameters(self):
         self.model_name = 'bert-large-cased'
         self.max_length = 128
-        self.batch_size = 64
+        self.eval_batch_size = 128
 
     def get_mapping_name(self):
         return "bert_vanilla_large"
