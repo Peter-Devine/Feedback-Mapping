71c211abfab987e0b05fc3f274fdbe436161d62b
unknown <pdev438@aucklanduni.ac.nz>
2020-08-19 16:29:31 +1200

    Update run.py

--- run.py
+++ run.py
@@ -2,16 +2,56 @@ from data.downloader import get_data
 
 from mapping.mapper import map_data
 
-from visualisation.visualise import visualise_data
-
-from clustering.clusterer import cluster_data
+from embed_evaluation.embed_evaluator import eval_embeds
 
 from results.score_collator import collate_scores
 
-from utils.utils import get_random_seed, set_random_seed
+from utils.utils import set_random_seed
 
 datasets = ["guzman_2015", "maalej_2016", "williams_2017", "chen_2014", "di_sorbo_2016", "scalabrino_2017", "jha_2017", "tizard_2019"]
-embeddings = ["use",  "tfidf_pca", "glove", "bert_vanilla", "lda", "gem"]#"tfidf_pca", "glove", "sbert_wk", "ensemble"]
+embeddings = [
+ #'albert_vanilla',
+ 'bart_vanilla',
+ 'bert_vanilla_first_tok',
+ 'bert_vanilla_large',
+ 'bert_vanilla',
+ 'distilbert_vanilla',
+ 'declutr_vanilla',
+ 'roberta_vanilla_large',
+ 'sentence_transformer_bert',
+ 'sentence_transformer_distilbert',
+ 'sentence_transformer_roberta',
+ 'bert_cls_trained_sublabel_outside',
+ 'bert_masking_trained_outside',
+ 'bert_nsp_cos_trained_outside',
+ 'bert_nsp_trained_outside',
+ 'gem',
+ 'glove',
+ 'keyword_matching',
+ 'random',
+ 'sbert_wk',
+ 'sbert',
+ 't5_vanilla',
+ #'use',
+ 'gsdmm',
+ 'gsdmm_small',
+ 'lda',
+ 'lda_small',
+ 'tfidf_pca',
+ 'tfidf_pca_small',
+ 'bert_cls_trained_mtl_except',
+ 'bert_masking_trained_app',
+ #'bert_masking_trained_dataset',
+ 'bert_masking_trained_mtl',
+ 'bert_cls_trained_sublabel_app',
+ #'bert_cls_trained_sublabel_dataset',
+ 'bert_cls_trained_sublabel_mtl',
+ 'bert_nsp_cos_trained_app',
+ #'bert_nsp_cos_trained_dataset',
+ 'bert_nsp_cos_trained_mtl',
+ 'bert_nsp_trained_app',
+ #'bert_nsp_trained_dataset',
+ 'bert_nsp_trained_mtl']
 
 set_random_seed(111)
 
@@ -19,8 +59,10 @@ get_data(datasets)
 
 map_data(datasets, embeddings)
 
-visualise_data(datasets, embeddings)
+eval_embeds()
 
-cluster_data(datasets, embeddings)
+collate_scores("5_nn_sim")
 
-collate_scores()
+collate_scores("5_cos_nn_sim")
+
+collate_scores("p_val")
